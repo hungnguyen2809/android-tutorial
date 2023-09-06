353c7a4ce5e64b3e844fd922ab7b5a9bbecec40b
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2023-09-06 10:00:06 +0700

    16

--- AndroidFirebase/app/src/main/java/com/hungnv28/androidfirebase/LoginActivity.java
+++ AndroidFirebase/app/src/main/java/com/hungnv28/androidfirebase/LoginActivity.java
@@ -7,18 +7,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.hungnv28.androidfirebase.databinding.ActivityLoginBinding;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
     private FirebaseAuth auth;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +37,39 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
-        setEventBtnSingUp();
-        setEventEdtEmail();
-        setEventEdtPassword();
         setEventTxtLogin();
+        setEventEdtPassword();
+        setEventEdtUsername();
+        setEventBtnSingUp();
     }
+
+    private void setEventEdtUsername() {
+        binding.edtUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence text, int start, int before, int count) {
+                if (text.toString().trim().length() != 0) {
+                    binding.edtUsername.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
 
     private void setEventTxtLogin() {
         binding.signRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
             }
         });
     }
@@ -52,7 +83,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence text, int start, int before, int count) {
                 if (text.toString().trim().length() != 0) {
-                    binding.edtPassword.setError("");
+                    binding.edtPassword.setError(null);
                 }
             }
 
@@ -62,34 +93,16 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    private void setEventEdtEmail() {
-        binding.edtEmail.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence text, int start, int before, int count) {
-                if (text.toString().trim().length() != 0) {
-                    binding.edtEmail.setError("");
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-        });
-    }
 
     private void setEventBtnSingUp() {
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = binding.edtEmail.getText().toString().trim();
+                String user = binding.edtUsername.getText().toString().trim();
                 String pass = binding.edtPassword.getText().toString().trim();
 
-                if (email.isEmpty()) {
-                    binding.edtEmail.setError("Please enter email");
+                if (user.isEmpty()) {
+                    binding.edtUsername.setError("Please enter email");
                     return;
                 }
                 if (pass.isEmpty()) {
@@ -97,20 +110,51 @@ public class LoginActivity extends AppCompatActivity {
                     return;
                 }
 
-                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                DatabaseReference reference = database.getReference("users");
+                Query query = reference.orderByChild("username").equalTo(user);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
-
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(LoginActivity.this, "Login error: " + task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (!snapshot.exists()) {
+                            binding.edtUsername.setError("Username not exists");
+                            binding.edtUsername.requestFocus();
+                            return;
                         }
+
+                        binding.edtUsername.setError(null);
+                        String passFormDb = snapshot.child(user).child("password").getValue(String.class);
+
+                        if (!Objects.equals(passFormDb, pass)) {
+                            binding.edtPassword.setError("Password is wrong");
+                            binding.edtPassword.requestFocus();
+                            return;
+                        }
+
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
                     }
                 });
 
+
+//                auth.signInWithEmailAndPassword(email, pass).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+//                    @Override
+//                    public void onSuccess(AuthResult authResult) {
+//                        Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+//
+//                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
             }
         });
     }
--- AndroidFirebase/app/src/main/java/com/hungnv28/androidfirebase/SignUpActivity.java
+++ AndroidFirebase/app/src/main/java/com/hungnv28/androidfirebase/SignUpActivity.java
@@ -7,19 +7,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.hungnv28.androidfirebase.databinding.ActivitySignUpBinding;
+import com.hungnv28.androidfirebase.models.Person;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private ActivitySignUpBinding binding;
     private FirebaseAuth auth;
+
+    private FirebaseDatabase database;
 
 
     @Override
@@ -29,11 +34,52 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
 
         setEventBtnSingUp();
         setEventEdtEmail();
+        setEventEdtFullName();
+        setEventEdtUsername();
         setEventEdtPassword();
         setEventTxtLogin();
+    }
+
+    private void setEventEdtUsername() {
+        binding.edtUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence text, int start, int before, int count) {
+                if (text.toString().trim().length() != 0) {
+                    binding.edtUsername.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private void setEventEdtFullName() {
+        binding.edtUsername.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence text, int start, int before, int count) {
+                if (text.toString().trim().length() != 0) {
+                    binding.edtUsername.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
     }
 
     private void setEventTxtLogin() {
@@ -54,7 +100,7 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence text, int start, int before, int count) {
                 if (text.toString().trim().length() != 0) {
-                    binding.edtPassword.setError("");
+                    binding.edtPassword.setError(null);
                 }
             }
 
@@ -73,7 +119,7 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence text, int start, int before, int count) {
                 if (text.toString().trim().length() != 0) {
-                    binding.edtEmail.setError("");
+                    binding.edtEmail.setError(null);
                 }
             }
 
@@ -87,11 +133,25 @@ public class SignUpActivity extends AppCompatActivity {
         binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String fullName = binding.edtFullName.getText().toString().trim();
                 String email = binding.edtEmail.getText().toString().trim();
+                String user = binding.edtUsername.getText().toString().trim();
                 String pass = binding.edtPassword.getText().toString().trim();
 
+                if (fullName.isEmpty()) {
+                    binding.edtFullName.setError("Please enter full name");
+                    return;
+                }
                 if (email.isEmpty()) {
                     binding.edtEmail.setError("Please enter email");
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    binding.edtEmail.setError("Email invalid");
+                    return;
+                }
+                if (user.isEmpty()) {
+                    binding.edtPassword.setError("Please enter user name");
                     return;
                 }
                 if (pass.isEmpty()) {
@@ -99,9 +159,11 @@ public class SignUpActivity extends AppCompatActivity {
                     return;
                 }
 
-                auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                Person person = new Person(fullName, email, user, pass);
+                DatabaseReference reference = database.getReference("users");
+                reference.child(user).setValue(person).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
+                    public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(SignUpActivity.this, "Sing up Successfully", Toast.LENGTH_SHORT).show();
 
@@ -112,6 +174,20 @@ public class SignUpActivity extends AppCompatActivity {
                         }
                     }
                 });
+
+//                auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            Toast.makeText(SignUpActivity.this, "Sing up Successfully", Toast.LENGTH_SHORT).show();
+//
+//                            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+//                            startActivity(intent);
+//                        } else {
+//                            Toast.makeText(SignUpActivity.this, "Sing up error: " + task.getException().toString(), Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
 
             }
         });
